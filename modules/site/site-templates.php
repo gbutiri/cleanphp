9662bde1ec9394cbb69160b3e4bc537955741ebf
George Butiri <MovieMaker713@gmail.com>
2014-10-03 16:16:39 -0700

    vertical form CSS completed.

--- modules/site/site-templates.php
+++ modules/site/site-templates.php
@@ -19,25 +19,25 @@ function renderSampleForm() {
         <p>This is the content that will appear while trying to ajax it in.</p>
         <div class="row">
             <div class="field-wrapper">
-                <label>Username</label>
+                <label class="arrow-right">Username</label>
                 <input type="text" placeholder="text field sample" name="textfield" />
             </div>
         </div>
         <div class="row">
             <div class="field-wrapper">
-                <label>Password</label>
+                <label class="arrow-right">Password</label>
                 <input type="password" placeholder="password" name="password" />
             </div>
         </div>
         <div class="row">
             <div class="field-wrapper">
-                <label>Description</label>
+                <label class="arrow-right">Description</label>
                 <textarea placeholder="description" name="description"></textarea>
             </div>
         </div>
         <div class="row">
             <div class="field-wrapper">
-                <label>Select One</label>
+                <label class="arrow-right">Select One</label>
                 <select name="selectone">
                     <option value=""> - Make your selection - </option>
                     <option value="1">Clear</option>
@@ -48,17 +48,17 @@ function renderSampleForm() {
         </div>
         <p>Do you like... </p>
         <div class="row">
-            <div class="field-wrapper checkbox">
+            <div class="checkbox">
                 <input type="checkbox" name="likes['reading']" value="true" class="hidden">
                 <i class="fa checkme fa-square-o" data-name="likes['reading']"></i>
                 <label>Reading?</label>
             </div>
-            <div class="field-wrapper checkbox">
+            <div class="checkbox">
                 <input type="checkbox" name="likes['writing']" value="true" class="hidden" />
                 <i class="fa checkme fa-square-o" data-name="likes['writing']"></i>
                 <label>Writing?</label>
             </div>
-            <div class="field-wrapper checkbox">
+            <div class="checkbox">
                 <input type="checkbox" name="likes['singing']" value="true" class="hidden" />
                 <i class="fa checkme fa-square-o" data-name="likes['singing']"></i>
                 <label>Singing?</label>
@@ -66,17 +66,17 @@ function renderSampleForm() {
         </div>
         <p>Are you awesome?</p>
         <div class="row">
-            <div class="field-wrapper radio">
+            <div class="radio">
                 <input type="radio" name="radio_awesome" value="Yes" class="hidden" />
                 <i class="fa radiome fa-circle-o" data-name="radio_awesome"></i>
                 <label>Yes</label>
             </div>
-            <div class="field-wrapper radio">
+            <div class="radio">
                 <input type="radio" name="radio_awesome" value="No" class="hidden" />
                 <i class="fa radiome fa-circle-o" data-name="radio_awesome"></i>
                 <label>No</label>
             </div>
-            <div class="field-wrapper radio">
+            <div class="radio">
                 <input type="radio" name="radio_awesome" value="Maybe" class="hidden" />
                 <i class="fa radiome fa-circle-o" data-name="radio_awesome"></i>
                 <label>Maybe</label>
@@ -84,14 +84,14 @@ function renderSampleForm() {
         </div>
         <p>Would you like us to remember you next time?</p>
         <div class="row">
-            <div class="field-wrapper checkbox">
+            <div class="checkbox">
                 <input type="checkbox" name="rememberme" value="true" class="hidden" />
                 <i class="fa checkme fa-square-o" data-name="rememberme"></i>
                 <label>Remember me!</label>
             </div>
         </div>
         <div class="row">
-            <div class="field-wrapper">
+            <div class="">
                 <input type="submit" value="Do Action" />
             </div>
         </div>
