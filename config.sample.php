1f30c4a41a743a4b8a4bd286c5ded15c83085369
George Butiri <MovieMaker713@gmail.com>
2014-10-03 11:08:04 -0700

    Updated some documentation and default config file.

--- config.sample.php
+++ config.sample.php
@@ -3,14 +3,14 @@ global $config;
 define("_ENV",'local');
 
 define("_DBHOST",'localhost');
-define("_DBUSER",'username');
-define("_DBPASS",'password');
+define("_DBUSER",'root');
+define("_DBPASS",'');
 define("_DBNAME",'dbname');
 
-define ("_SITE",'http://dev.site.com');
+define ("_SITE",'http://local.cleanphp.com');
 define ("_DOCROOT",$_SERVER['DOCUMENT_ROOT']);
 if (isset($_SESSION['site_username']) && $_SESSION['site_username'] != '') {
-	define("_USERNAME",$_SESSION['actingshowcase_cdp_username']);
+	define("_USERNAME",$_SESSION['site_username']);
 } else {
 	define("_USERNAME",null);
 }
