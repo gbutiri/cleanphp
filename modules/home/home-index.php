f10dab3ecd8a79a694f8a8389cd2e1d0026d23f9
George Butiri <MovieMaker713@gmail.com>
2014-10-22 17:58:10 -0700

    minor

--- modules/home/home-index.php
+++ modules/home/home-index.php
@@ -1,5 +1,6 @@
 <h2>Home Module</h2>
 <a href="#" class="tmbtn" data-module="home" data-action="load_module_text">Load Module Text</a>
 <p id="replace-me">This will be replaced</p>
+
 <div contenteditable="true" placeholder="Enter text here" data-action="save_large_textarea" class="autoheight autosave"></div>
 <div contenteditable="true" placeholder="Enter text here for error" data-action="save_large_textarea2" class="autoheight autosave"></div>
